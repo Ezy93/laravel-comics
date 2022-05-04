a7c9ff7ff0d4b72c7521f8ead6c7e6155c8e2d20
Ezy93 <giordani001@gmail.com>
2022-05-04 19:42:29 +0200

    aggiunto file config per i links del footer

---
+++ config/topFooterLinks.php
@@ -0,0 +1,65 @@
+<?php  
+    return[
+        'dcComicsLinks' => [
+            "Character",
+            "Comics",
+            "Movies",
+            "TV",
+            "Games",
+            "Videos",
+            "News"
+        ],
+        'shopLinks'=>[
+            "Shop DC",
+            "Shop DC Collectibles",
+        ],
+        'dcLinks'=>[
+            "Terms of Use",
+            "Privacy policy(New)",
+            "Ad Choices",
+            "Advertising",
+            "Jobs",
+            "Subscriptions",
+            "Talent Workshop",
+            "CPCS Certificates",
+            "Ratings",
+            "Shop Help",
+            "Contact Us"
+        ],
+        'sitesLinks'=>[
+            "DC",
+            "Mad magazine",
+            "DC kids",
+            "DC universe",
+            "DC Power Visa",
+        ],
+        'footerLinks'=>[
+            [
+                'name'=>'facebook icon',
+                'img'=>'footer-facebook.png',
+                'url'=>'#'
+            ],
+            [
+                'name'=>'twitter icon',
+                'img'=>'footer-twitter.png',
+                'url'=>'#'
+            ],
+            [
+                'name'=>'youtube icon',
+                'img'=>'footer-youtbe.png',
+                'url'=>'#'
+            ],
+            [
+                'name'=>'pinterest icon',
+                'img'=>'footer-pinterest.png',
+                'url'=>'#'
+            ],
+            [
+                'name'=>'periscope icon',
+                'img'=>'footer-periscope.png',
+                'url'=>'#'
+            ],
+            
+        ]
+    ]
+?>
