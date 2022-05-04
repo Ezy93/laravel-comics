7557dbc629a33ef5d5a8cae7d5f7a8963cda2039
Ezy93 <giordani001@gmail.com>
2022-05-04 19:47:53 +0200

    aggiunto file config per data nel main

---
+++ config/shopLinks.php
@@ -0,0 +1,29 @@
+<?php  
+    return[
+        [
+            'name'=>"digital comics",
+            'img'=> "buy-comics-digital-comics.png",
+            'url'=>"#",
+        ],
+        [
+            'name'=>"dc merchandise",
+            'img'=>"buy-comics-merchandise.png",
+            'url'=>'#'
+        ],
+        [
+            'name'=>"subscription",
+            'img'=>"buy-comics-subscriptions.png",
+            'url'=>'#'
+        ],
+        [
+            'name'=>"comic shop locator",
+            'img'=>"buy-comics-shop-locator.png",
+            'url'=>'#'
+        ],
+        [
+            'name'=>"dc power visa",
+            'img'=>"buy-dc-power-visa.svg",
+            'url'=>'#'
+        ],
+    ]
+?>
